6329a6f78efbf21b873a3799ababe65910bc315f
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-07-28 13:43:51 +0900

    [Edit] Modified app structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import { useState } from 'react'
+import styled from 'styled-components';
+import { GlobalStyle } from './GlobalStyle'
+import Window from './Components/Window'
+//import OpenedLayers from './Components/OpenedLayers'
+import Layer from './Components/Layer'
 
+const MainWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(rgb(255, 180, 212), rgb(166,197, 244));
+`
 function App() {
+  const [openMyFolder, setOpenMyFolder] = useState(false)
+  const toggleMyFolder = () => {
+    setOpenMyFolder(!openMyFolder)
+  }
+
   return (
-    <div className="App">
-      hello world
-    </div>
+    <>
+      <GlobalStyle/>
+      <MainWrapper>
+        <Window openMyFolder={openMyFolder} showMyFolder={toggleMyFolder} />
+        {openMyFolder && <Layer closeMyFolder={toggleMyFolder} />}
+      </MainWrapper>
+    </>
   );
 }
 
