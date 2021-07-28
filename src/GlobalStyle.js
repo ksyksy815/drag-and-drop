ca4b23f2104b82cf78f3ef6f4d78e92f4cc6d4cb
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-07-28 17:03:00 +0900

    [Edit] Added font families

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -5,5 +5,14 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Nanum Gothic', sans-serif;
+    color: #444444;
   }
 `
+
+/*
+font-family: 'Do Hyeon', sans-serif;
+font-family: 'Jua', sans-serif;
+font-family: 'Nanum Gothic', sans-serif;
+font-family: 'Poppins', sans-serif;
+*/
